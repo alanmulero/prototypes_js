48dc1f9a6a1b7710eb51540bf8618439626669f7
alan.mulero <alan.mulero@gmail.com>
2023-02-22 14:18:22 -0300

    Criando um prototype e uma funcao para depositar na conta poupanca

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function Cliente(nome,idade,saldo){
 const alan = new Cliente('Alan',54,100);
 console.log(alan);
 console.log(typeof alan);
+// Construindo um novo objedo baseado em Cliente, agora uma conta poupança.
+function ClientePoupanca(nome,idade,saldo,saldoPoupanca){
+	Cliente.call(this,nome,idade,saldo);
+	this.saldoPoupanca = saldoPoupanca;
+	console.log(`O saldo da poupanca: ${this.saldoPoupanca}`);
+
+}
+// Criando uma instancia para conta poupanca
+const maria = new ClientePoupanca('Maria',40,110,200);
+console.log(maria);
+// Criando uma funcao para deposito em conta poupanca
+ClientePoupanca.prototype.depositarPoupanca = function(valor){
+	this.saldoPoupanca += valor;
+	console.log(`O saldo da poupanca depois do deposito: ${this.saldoPoupanca}`)
+}
+maria.depositarPoupanca(12);
